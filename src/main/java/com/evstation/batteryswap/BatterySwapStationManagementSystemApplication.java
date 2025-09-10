cbe45e5f3b6f2bf81c8883aacae1d59c18140f33
maitrung0104 <maitrung0104@gmail.com>
2025-09-10 11:32:21 +0700

    Initial commit

---
+++ src/main/java/com/evstation/batteryswap/BatterySwapStationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.evstation.batteryswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterySwapStationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatterySwapStationManagementSystemApplication.class, args);
+	}
+
+}
