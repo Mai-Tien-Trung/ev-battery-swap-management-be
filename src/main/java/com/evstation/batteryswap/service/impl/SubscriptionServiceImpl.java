1a8daaca8480eab5326e10531aa182c72f31066c
maitrung0104 <maitrung0104@gmail.com>
2025-11-08 15:45:52 +0700

    Update Subscription name

--- src/main/java/com/evstation/batteryswap/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/evstation/batteryswap/service/impl/SubscriptionServiceImpl.java
@@ -114,7 +114,7 @@ public class SubscriptionServiceImpl implements SubscriptionService {
                     : currentPlan;
 
             return new SubscriptionDetailResponse(
-                    vehicle.getModel() + " " + vehicle.getVin(),
+                    vehicle.getModel().getName() + " " + vehicle.getVin(),
                     currentPlan.getName(),
                     sub.getStartDate(),
                     sub.getEndDate(),
